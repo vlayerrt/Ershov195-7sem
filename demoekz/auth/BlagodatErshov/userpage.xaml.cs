493558676c58acf884c7eeca1ee92212c7c8149d
vlad <vladislav27.022@gmail.com>
2022-12-07 15:21:35 +0300

    БлагодатьАвторизация

---
+++ demoekz/auth/BlagodatErshov/userpage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlagodatErshov
+{
+    /// <summary>
+    /// Логика взаимодействия для adminpage.xaml
+    /// </summary>
+    public partial class userpage : Page
+    {
+        public userpage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var currentUser = App.Context.Users.FirstOrDefault(p => p.login == loginBox.Text && p.password == passwordBox.Password);
+
+            if (currentUser != null)
+            {
+                App.CurrentUser = currentUser;
+            }
+            else if (currentUser != null)
+            {
+                App.CurrentUser = currentUser;
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (App.CurrentUser.RoleID == 1)
+            {
+                NavigationService.Navigate(new adminpage());
+
+            }
+            else if (App.CurrentUser.RoleID == 2)
+            {
+                NavigationService.Navigate(new ServicesPage());
+            }
+        }
+    }
+}
