1f52cb232582d4eab3adddb7b72766d3d439043f
vlad <vladislav27.022@gmail.com>
2022-12-07 15:19:52 +0300

    ГлазкиАвторизация

---
+++ demoekz/auth/Глазки-SaveErshov/Administrator.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Глазки_SaveErshov
+{
+    /// <summary>
+    /// Логика взаимодействия для Administrator.xaml
+    /// </summary>
+    public partial class Administrator : Page
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        DateTime date = new DateTime(0, 0);
+        public Administrator()
+        {
+            InitializeComponent();
+            Classes.ConnectDB.modeldb = new model.GlazkiSaveEVEntities();
+
+            UserTB.Text = model.GlazkiSaveEVEntities.currentUser.login;
+            
+
+
+
+            RoleTB.Text = "(" + model.GlazkiSaveEVEntities.currentUser.Role.RoleName + ")";
+
+            /*  var fullFilePath = App.modeldb.CurentStaff.Photo;
+
+              BitmapImage bitmap = new BitmapImage();
+              bitmap.BeginInit();
+              bitmap.UriSource = new Uri(fullFilePath, UriKind.Relative);
+              bitmap.EndInit();
+
+              UserPhoto.Source = bitmap;
+  */
+        }
+    }
+}
---
+++ demoekz/auth/Глазки-SaveErshov/MainWindowGlazki.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Глазки_SaveErshov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    
+
+    public partial class MainWindowGlazki : Window
+    {
+        public MainWindowGlazki()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Admin());
+            Classes.manager.MainFraim = MainFrame;
+        }
+
+        private void Admin_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Admin());
+        }
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new User());
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.GoBack();
+        }
+
+        private void MainFrame_Back(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack) Back_btn.Visibility = Visibility.Visible;
+            else Back_btn.Visibility = Visibility.Hidden;
+        }
+    }
+}
