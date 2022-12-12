63af13647e03ec5d8c952cedc8558a18b28b8e2f
vlad <vladislav27.022@gmail.com>
2022-12-13 00:07:39 +0300

    Add files via upload

---
+++ demoekz/register/ToursAgencyEV195/DataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ToursAgencyEV195
+{
+    internal class DataBase
+    {
+
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=VLAYERPC; Initial Catalog=ToursAgencyEV195; Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
